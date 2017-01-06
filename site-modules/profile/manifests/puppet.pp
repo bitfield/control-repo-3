763e39fbe784c6b5fa8e5d82ec43a172fb495bde
John Arundel <john@bitfieldconsulting.com>
2017-01-06 13:20:35 +0000

    Add more Puppet scripts and resources

--- site-modules/profile/manifests/puppet.pp
+++ site-modules/profile/manifests/puppet.pp
@@ -1,11 +1,43 @@
-# Set up Puppet config and updates
+# Set up Puppet config and cron run
 class profile::puppet {
   file { '/etc/puppetlabs/puppet/puppet.conf':
     source => 'puppet:///modules/profile/puppet/puppet.conf',
   }
 
+  service { ['puppet', 'mcollective', 'pxp-agent']:
+    ensure  => stopped, # Puppet runs from cron
+    enable  => false,
+  }
+
+  cron { 'run-puppet':
+    ensure  => present,
+    command => '/usr/local/bin/run-puppet',
+    minute  => '*/10',
+    hour    => '*',
+  }
+
+  file { '/usr/local/bin/run-puppet':
+    source => 'puppet:///modules/profile/puppet/run-puppet.sh',
+    mode   => '0755',
+  }
+
   file { '/usr/local/bin/papply':
     source => 'puppet:///modules/profile/puppet/papply.sh',
     mode   => '0755',
+  }
+
+  file { '/usr/local/bin/plock':
+    source => 'puppet:///modules/profile/puppet/plock.sh',
+    mode   => '0755',
+  }
+
+  file { '/usr/local/bin/punlock':
+    source => 'puppet:///modules/profile/puppet/punlock.sh',
+    mode   => '0755',
+  }
+
+  file { '/tmp/puppet.lastrun':
+    content => strftime('%F %T'),
+    backup  => false,
   }
 }
