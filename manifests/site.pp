2145b758aa7260f95b3c3f3c49a7df621a53328a
John Arundel <john@bitfieldconsulting.com>
2017-01-06 12:55:58 +0000

    Add demo node

--- manifests/site.pp
+++ manifests/site.pp
@@ -14,3 +14,7 @@
 # Disable filebucket by default for all File resources:
 #https://docs.puppet.com/pe/2015.3/release_notes.html#filebucket-resource-no-longer-created-by-default
 File { backup => false }
+
+node 'demo' {
+  include role::demo
+}
