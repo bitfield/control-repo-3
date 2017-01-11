41cd75e823c54f8da0f54899f88c59c07449c900
John Arundel <john@bitfieldconsulting.com>
2017-01-11 14:46:12 +0000

    Have run-puppet run r10k puppetfile install to update modules

--- site-modules/profile/files/puppet/run-puppet.sh
+++ site-modules/profile/files/puppet/run-puppet.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /etc/puppetlabs/code/environments/production && git pull
+/opt/puppetlabs/puppet/bin/r10k puppetfile install
 /opt/puppetlabs/bin/puppet apply --environment production manifests/*
