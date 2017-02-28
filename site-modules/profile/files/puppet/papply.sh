ede6996655a569987408cca4f72c1f0430328014
John Arundel <john@bitfieldconsulting.com>
2017-02-28 11:30:47 +0000

    Change strict=error to warning (timezone complains)

--- site-modules/profile/files/puppet/papply.sh
+++ site-modules/profile/files/puppet/papply.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 environment=${ENVIRONMENT:-production}
-/opt/puppetlabs/bin/puppet apply --environment ${environment} --strict=error /etc/puppetlabs/code/environments/${environment}/manifests/* $*
+/opt/puppetlabs/bin/puppet apply --environment ${environment} --strict=warning /etc/puppetlabs/code/environments/${environment}/manifests/* $*
