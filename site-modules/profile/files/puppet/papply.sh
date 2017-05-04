aeeceff304ecb08ec0c4b4f4788c15e4bfb6ad5d
John Arundel <john@bitfieldconsulting.com>
2017-05-04 10:51:10 +0100

    Run r10k install in papply

--- site-modules/profile/files/puppet/papply.sh
+++ site-modules/profile/files/puppet/papply.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 environment=${ENVIRONMENT:-production}
+/opt/puppetlabs/puppet/bin/r10k puppetfile install
 /opt/puppetlabs/bin/puppet apply --environment ${environment} --strict=warning /etc/puppetlabs/code/environments/${environment}/manifests/* $*
