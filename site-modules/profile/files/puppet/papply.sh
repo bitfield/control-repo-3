2f4075dd7ab2a31af45603620c8a1e44542db10b
John Arundel <john@bitfieldconsulting.com>
2017-05-23 16:44:52 +0100

    Avoid using the name ENVIRONMENT for a variable

--- site-modules/profile/files/puppet/papply.sh
+++ site-modules/profile/files/puppet/papply.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-environment=${ENVIRONMENT:-production}
+environment=${PUPPET_ENV:-production}
 /opt/puppetlabs/puppet/bin/r10k puppetfile install
 /opt/puppetlabs/bin/puppet apply --environment ${environment} --strict=warning /etc/puppetlabs/code/environments/${environment}/manifests/* $*
