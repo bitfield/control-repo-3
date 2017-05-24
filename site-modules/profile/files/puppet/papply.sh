f8f7a5858531f4f20fafd16e2177a90a681d3b37
John Arundel <john@bitfieldconsulting.com>
2017-05-24 12:43:25 +0100

    cd to puppet directory before r10k run

--- site-modules/profile/files/puppet/papply.sh
+++ site-modules/profile/files/puppet/papply.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 environment=${PUPPET_ENV:-production}
+cd /etc/puppetlabs/code/environments/${environment}
 /opt/puppetlabs/puppet/bin/r10k puppetfile install
 /opt/puppetlabs/bin/puppet apply --environment ${environment} --strict=warning /etc/puppetlabs/code/environments/${environment}/manifests/* $*
