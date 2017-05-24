3ae1828b4bc47bacec49a5692422ac6ee2d04ce7
John Arundel <john@bitfieldconsulting.com>
2017-05-24 14:11:26 +0100

    Apply manifest directory, not shell glob

--- site-modules/profile/files/puppet/papply.sh
+++ site-modules/profile/files/puppet/papply.sh
@@ -2,4 +2,4 @@
 environment=${PUPPET_ENV:-production}
 cd /etc/puppetlabs/code/environments/${environment}
 /opt/puppetlabs/puppet/bin/r10k puppetfile install
-/opt/puppetlabs/bin/puppet apply --environment ${environment} --strict=warning /etc/puppetlabs/code/environments/${environment}/manifests/* $*
+/opt/puppetlabs/bin/puppet apply --environment ${environment} --strict=warning /etc/puppetlabs/code/environments/${environment}/manifests/ $*
