ebe57213e8c7ecb7fe635e9cae653c197ebae6ea
John Arundel <john@bitfieldconsulting.com>
2017-07-11 13:16:01 +0100

    Update Vagrant setup

--- scripts/vagrant_provision.sh
+++ scripts/vagrant_provision.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-apt-key adv --fetch-keys http://apt.puppetlabs.com/DEB-GPG-KEY-puppet
+source /etc/lsb-release
+wget https://apt.puppetlabs.com/puppet5-release-${DISTRIB_CODENAME}.deb
+dpkg -i puppet5-release-${DISTRIB_CODENAME}.deb
 apt-get update
-apt-get -y install puppet-agent
+apt-get -y install git puppet-agent
+echo 'Defaults        secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/opt/puppetlabs/bin"' >/etc/sudoers.d/puppet
+/opt/puppetlabs/puppet/bin/gem install gpgme --no-rdoc --no-ri
+/opt/puppetlabs/puppet/bin/gem install hiera-eyaml-gpg --no-rdoc --no-ri
 /opt/puppetlabs/puppet/bin/gem install r10k --no-rdoc --no-ri
 cd /etc/puppetlabs/code/environments/production
 /opt/puppetlabs/puppet/bin/r10k puppetfile install --verbose
