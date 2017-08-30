03b5247a9fdf738bd36890bf3ea5a2614c8dfa35
John Arundel <john@bitfieldconsulting.com>
2017-08-30 11:53:27 +0100

    Add /opt/puppetlabs/bin to secure_path

--- site-modules/profile/manifests/sudoers.pp
+++ site-modules/profile/manifests/sudoers.pp
@@ -1,7 +1,7 @@
 # Manage user privileges
 class profile::sudoers {
   sudo::conf { 'secure_path':
-    content  => 'Defaults      secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/puppetlabs/puppet/bin"',
+    content  => 'Defaults      secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/puppetlabs/puppet/bin:/opt/puppetlabs/bin"',
     priority => 0,
   }
   $sudoers = lookup('sudoers', Array[String], 'unique', [])
